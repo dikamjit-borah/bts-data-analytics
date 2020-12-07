c1a1f8978ed923905dddf488e10a6319fc78e944
Dikamjit Borah <36004021+hunkyxstudman@users.noreply.github.com>
2020-12-07 23:38:17 +0530

    Update maingit.py

--- maingit.py
+++ maingit.py
@@ -5,6 +5,8 @@ import csv
 import re 
 import string
 import preprocessor as p
+import time
+import random
 
 auth = tweepy.OAuthHandler("", "")
 auth.set_access_token("", "")
@@ -16,24 +18,66 @@ api = tweepy.API(auth)
 #     print(tweet.text.encode('utf-8'))
 
 
-csvFile = open('file-name_2', 'a')
+csvFile = open('1825 01-12 KEY=KPOPorBTS.csv', 'a', newline='')
+csvFile2 = open('bts_world2.csv', 'a', newline='')
 csvWriter = csv.writer(csvFile)
- 
-search_words = "bts"      # enter your words
+csvWriter2 = csv.writer(csvFile2)
+
+search_words = "kpop OR bts"   #grumpy OR cat OR #meme  
 new_search = search_words + " -filter:retweets"
-COUNT = 0
-HASHTAGS = ""
+count_country = 0
+print("Fetching...")
+substring = "India"
 
+#tweets = api.search(keyword, count=10, lang='en', exclude='retweets',tweet_mode='extended')
 
-for tweet in tweepy.Cursor(api.search,q=new_search,count=100,lang="en",since_id=0, exclude='retweets').items():
-    COUNT = COUNT + 1
-    if(COUNT !=10):
-     
-        if(tweet.user.location.encode('utf-8')!="b''" or tweet.user.location.encode('utf-8')!="b'' "):
-            print(tweet.user.location.encode('utf-8'))
-        # for i in tweet.entities.hashtags:
-        #     HASHTAGS += str(i) + ","
-        if(tweet.user.location!="''"):
-            csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8'), tweet.user.name.encode('utf-8')])
+count_total =0
+for tweet in tweepy.Cursor(api.search,q=new_search,geocode="20.5937,78.9629,2933km",count=100,lang="en",since_id=0, exclude='retweets', geo_enabled=True, wait_on_rate_limit=True, wait_on_rate_limit_notify=True).items():
+    if(count_total<20000):
+        count_total = count_total + 1
+        print("####################################")
+        print("{} -> {}".format(count_total, tweet.id_str))
+        #time.sleep(random.randint(2, 8))
+        T_ID=tweet.id_str
+        T_TEXT = tweet.text.encode('utf-8')
+        T_DATETIME = tweet.created_at
+        T_USERNAME = tweet.user.name.encode('utf-8')
+        T_SCREENNAME = tweet.user.screen_name.encode('utf-8')
+        T_LOCATION = tweet.user.location.encode('utf-8')
+        T_FOLLOWERS = tweet.user.followers_count
+        T_FRIENDS = tweet.user.friends_count
+        T_STATUSES = tweet.user.statuses_count
+        T_HASHTAGS = []
+        T_MENTIONS = []
+        try:
+            for i in range(3):
+                T_HASHTAGS.append(tweet.entities["hashtags"][i]["text"])
+        except:
+            pass
+           
+        try:
+            for i in range(3):
+                T_MENTIONS.append(tweet.entities["user_mentions"][i]["screen_name"].encode('utf-8'))
+        except:
+            pass
+
+        if(count_country<10000):
+            #print(tweet)
+            #if(tweet.user.location.startswith("b''", 0, 3)==False):
+            #print(count_country)
+            try:
+                print("Writing in bts_india")    
+                csvWriter.writerow([T_ID, T_DATETIME, T_TEXT, T_HASHTAGS, T_MENTIONS, T_LOCATION, T_USERNAME, T_SCREENNAME, T_FOLLOWERS, T_FRIENDS, T_STATUSES ])
+                count_country = count_country + 1
+                print("bts_india comments = ",count_country)
+               
+            except:
+                pass
+            
+        else:
+            exit()
+        print("####################################")
+        print("")
     else:
         exit()
+
